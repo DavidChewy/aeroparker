67405b6428b12b8014f29ff12055971355baad6d
DavidChewy <davidchiu71@gmail.com>
2025-08-14 16:09:42 +0100

    Testing commit

---
+++ src/main/java/com/example/aeroparkertechtest/AeroparkerTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aeroparkertechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroparkerTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeroparkerTechnicalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aeroparkertechtest/AeroparkerTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aeroparkertechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroparkerTechnicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
